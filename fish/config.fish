cee4478955427bad08922f3e9510280ee79dcce5
luoqi <luoqi9421@163.com>
2025-02-26 19:37:14 +0800

    first commit

--- fish/config.fish
+++ fish/config.fish
@@ -31,6 +31,7 @@ end
 
 set -gx ALL_PROXY http://127.0.0.1:7897
 set -gx EDITOR nvim
+set -gx VISUAL neovide
 set -gx PATH $HOME/.local/bin $PATH
 set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 # fnm
--- hypr/UserScripts/QuickEdit.sh
+++ hypr/UserScripts/QuickEdit.sh
@@ -3,7 +3,8 @@
 # Rofi menu for Quick Edit/View of Settings (SUPER E)
 
 # Define preferred text editor and terminal
-edit=${EDITOR:-nano}
+# edit=${EDITOR:-nano}
+edit=neovide
 tty=kitty
 
 # Paths to configuration directories
@@ -14,7 +15,7 @@ msg=' ⁉️ Choose which config to View or Edit ⁉️'
 
 # Function to display the menu options
 menu() {
-    cat <<EOF
+  cat <<EOF
 1. ENV variables
 2. Window Rules
 3. Monitors
@@ -31,30 +32,30 @@ EOF
 
 # Main function to handle menu selection
 main() {
-    choice=$(menu | rofi -i -dmenu -config $rofi_theme -mesg "$msg" | cut -d. -f1)
-    
-    # Map choices to corresponding files
-    case $choice in
-        1) file="$UserConfigs/ENVariables.conf" ;;
-        2) file="$UserConfigs/WindowRules.conf" ;;
-        3) file="$UserConfigs/Monitors.conf" ;;
-        4) file="$UserConfigs/UserKeybinds.conf" ;;
-        5) file="$UserConfigs/UserSettings.conf" ;;
-        6) file="$UserConfigs/Startup_Apps.conf" ;;
-        7) file="$UserConfigs/UserDecorations.conf" ;;
-        8) file="$UserConfigs/UserAnimations.conf" ;;
-        9) file="$UserConfigs/WorkspaceRules.conf" ;;
-        10) file="$UserConfigs/Laptops.conf" ;;
-        11) file="$configs/Keybinds.conf" ;;
-        *) return ;;  # Do nothing for invalid choices
-    esac
+  choice=$(menu | rofi -i -dmenu -config $rofi_theme -mesg "$msg" | cut -d. -f1)
 
-    # Open the selected file in the terminal with the text editor
-    $tty -e $edit "$file"
+  # Map choices to corresponding files
+  case $choice in
+  1) file="$UserConfigs/ENVariables.conf" ;;
+  2) file="$UserConfigs/WindowRules.conf" ;;
+  3) file="$UserConfigs/Monitors.conf" ;;
+  4) file="$UserConfigs/UserKeybinds.conf" ;;
+  5) file="$UserConfigs/UserSettings.conf" ;;
+  6) file="$UserConfigs/Startup_Apps.conf" ;;
+  7) file="$UserConfigs/UserDecorations.conf" ;;
+  8) file="$UserConfigs/UserAnimations.conf" ;;
+  9) file="$UserConfigs/WorkspaceRules.conf" ;;
+  10) file="$UserConfigs/Laptops.conf" ;;
+  11) file="$configs/Keybinds.conf" ;;
+  *) return ;; # Do nothing for invalid choices
+  esac
+
+  # Open the selected file in the terminal with the text editor
+  $tty -e $edit "$file"
 }
 
 # Check if rofi is already running
-if pidof rofi > /dev/null; then
+if pidof rofi >/dev/null; then
   pkill rofi
 fi
 
